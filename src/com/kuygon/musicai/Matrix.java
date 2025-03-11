58a3bc5e7fcae3dd97ab09b597a4a1712dbef3e7
NathanAyre <nathanayre16@gmail.com>
2025-03-11 20:18:42 +0000

    added product and transpose (static) functions

--- src/com/kuygon/musicai/Matrix.java
+++ src/com/kuygon/musicai/Matrix.java
@@ -19,12 +19,41 @@ public class Matrix {
         }
     }
 
+    public static Matrix product (Matrix m1, Matrix m2) throws Exception {
+        if (m1.m[0].length != m2.m.length)
+            throw new Exception("M1 columns doesn't match M2 rows");
+        double[][] m3 = new double[m1.m.length][m2.m[0].length];
+
+        for (int row = 0; row < m1.m.length; row++) {
+            for (int col = 0; col < m2.m[0].length; col++) {
+                double cellValue = 0.0;
+                for (int cell = 0; cell < m1.m[row].length; cell++) {
+                    cellValue += m1.m[row][cell] * m2.m[cell][col];
+                }
+                m3[row][col] = cellValue;
+            }
+        }
+        return new Matrix(m3);
+    }
+
+    public static Matrix transpose (Matrix m1) {
+        double[][] m2 = new double[m1.m[0].length][m1.m.length];
+        for (int row = 0; row < m1.m.length; row++) {
+            for (int col = 0; col < m1.m[0].length; col++) {
+                m2[col][row] = m1.m[row][col];
+            }
+        }
+        return new Matrix(m2);
+    }
+
+
+
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder("[");
         for (int row = 0; row < m.length; row++) {
             if (row > 0)
-                s.append(",");
+                s.append(",\n");
             s.append("[");
             for (int col = 0; col < m[0].length; col++) {
                 if (col > 0)
--- src/com/kuygon/musicai/Test.java
+++ src/com/kuygon/musicai/Test.java
@@ -1,12 +1,16 @@
 package com.kuygon.musicai;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         double[][] m = {{1, 2, 3}, {4, 5, 6}};
         Matrix testMatrix = new Matrix(m);
-        System.out.println(testMatrix);
+        System.out.println(testMatrix + "\n");
 
         testMatrix.scale(10);
-        System.out.println(testMatrix);
+        System.out.println(testMatrix + "\n");
+        Matrix testMatrix2 = Matrix.transpose(testMatrix);
+        System.out.println(testMatrix2 + "\n");
+        Matrix productResult = Matrix.product(testMatrix, testMatrix2);
+        System.out.println(productResult + "\n");
     }
 }
